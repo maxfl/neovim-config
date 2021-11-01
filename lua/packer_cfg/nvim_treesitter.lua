416f74092197e49bbf145de9a4c37ce3da184112
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-11-01 12:01:28 +0300

    minor

--- lua/packer_cfg/nvim_treesitter.lua
+++ lua/packer_cfg/nvim_treesitter.lua
@@ -1,18 +1,35 @@
 return function()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = {'lua', 'python'}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-            ignore_install = {}, -- List of parsers to ignore installing
-            highlight = {
-                enable = true,              -- false will disable the whole extension
-                disable = { "c", "rust" },  -- list of language that will be disabled
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
+    require'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+            'lua', 'python', 'vim',
+            'bash', 'fish',
+            'cmake',
+            'cpp', 'julia',
+            'latex',
+            'rst',
+            'yaml'
+        },
+        ignore_install = {}, -- List of parsers to ignore installing
+        highlight = {
+            enable = true,              -- false will disable the whole extension
+            disable = { "c", "rust" },  -- list of language that will be disabled
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+        },
+        indent = {
+            enable = false,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "gnn",
+                node_incremental = "grn",
+                scope_incremental = "grc",
+                node_decremental = "grm",
             },
-            indent = {
-                enable = true,
-            }
-        }
-    end
+        },
+    }
+end
