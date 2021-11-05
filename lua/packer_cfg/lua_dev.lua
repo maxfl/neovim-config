acfaec804959a8fa79f278880a1d8aac36cb280a
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-11-05 12:24:44 +0300

    minor

--- lua/packer_cfg/lua_dev.lua
+++ lua/packer_cfg/lua_dev.lua
@@ -4,13 +4,16 @@ return {
     -- ft='lua',
     config=function()
         local luadev = require("lua-dev").setup{
+            vimruntime=true,
+            types=true,
+            plugins=true
             -- add any options here, or leave empty to use the default settings
             -- lspconfig = {
                 --   cmd = {"lua-language-server"}
                 -- },
             }
 
-            -- local lspconfig = require('lspconfig')
-            -- lspconfig.sumneko_lua.setup(luadev)
+            local lspconfig = require('lspconfig')
+            lspconfig.sumneko_lua.setup(luadev)
         end
     }
