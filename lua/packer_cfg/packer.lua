fcc9ed3c9248a1f8b07f4f0c18639b1be771c39d
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-11-12 12:05:00 +0300

    minor

--- lua/packer_cfg/packer.lua
+++ lua/packer_cfg/packer.lua
@@ -202,7 +202,7 @@ packer.startup(function()
         use (require 'packer_cfg.devdocs')
 
     -- Local plugins
-        use {"~/.config/nvim/bundle_local/SwapText-1.02", opt=true, keys={'', '<Leader>x'}, requires={'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}
+        use {"~/.config/nvim/bundle_local/SwapText-1.02", opt=true, keys={'n', '<Leader>x'}, requires={'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}
         use {'~/.config/nvim/bundle_local/txt-vim-syntax-1.2', opt=true, ft='txt'}
         use {'~/.config/nvim/bundle_local/txtfmt_v3.1', opt=true, ft='txt'}
     end
