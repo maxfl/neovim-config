fe7e4719b7141a7bd52645313f92dcdd76a06dfb
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-11-15 09:26:34 +0300

    minor

--- lua/packer_cfg/indent_blankline.lua
+++ lua/packer_cfg/indent_blankline.lua
@@ -1,17 +1,17 @@
 return {
     'lukas-reineke/indent-blankline.nvim',
     opt=true,
-    ft='python',
+    ft={'python', 'lua', 'fish'},
     cmd={'IndentBlanklineToggle', 'IndentBlankline*'},
     requires='nvim-treesitter/nvim-treesitter', -- needed for show_current_context
     config=function()
         require 'indent_blankline'.setup {
-            enabled=false,
+            enabled=true,
             -- char = "|",
             char = '┊',
             show_end_of_line=true,
             buftype_exclude = {"terminal"},
-            filetype={'python'},
+            filetype={'python', 'fish', 'lua'},
             show_current_context=true -- needs treesitter
         }
     end
