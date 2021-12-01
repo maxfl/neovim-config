2b63b92284b4066c89f32d4d4809bd3c47a63473
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-12-01 09:51:06 +0300

    minor

--- lua/packer_cfg/fugitive.lua
+++ lua/packer_cfg/fugitive.lua
@@ -5,8 +5,8 @@ return {
         local map = vim.api.nvim_set_keymap
         local silent={silent=true}
         map('n', '<Leader>ga', '<CMD>!git add %<CR>', silent)
-        map('n', '<Leader>gc', '<CMD>Git commit -a<CR>', silent)
-        map('n', '<Leader>gC', '<CMD>Git commit<CR>', silent)
+        map('n', '<Leader>gc', '<CMD>Git commit<CR>', silent)
+        map('n', '<Leader>gC', '<CMD>Git commit -a<CR>', silent)
         map('n', '<Leader>gp', '<CMD>Git push<CR>', silent)
         map('n', '<Leader>gs', '<CMD>Git status<CR>', silent)
         map('n', '<Leader>gP', '<CMD>Git pull<CR>', silent)
