ca8dc3ff80f9b4568700f80dfa01f6c2124c6b8e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-12-02 12:08:07 +0300

    minor

--- lua/packer_cfg/vim_easy_align.lua
+++ lua/packer_cfg/vim_easy_align.lua
@@ -9,12 +9,12 @@ return {
         local silent={silent=true}
         map('v', '<leader>ea',  '<Plug>(LiveEasyAlign)', silent)
         map('v', '<leader>eA',  '<Plug>(EasyAlign)', silent)
-        map('n', '<leader>et',  '<CMD>let p=getpos(".")<CR>vie<CMD>EasyAlign * &<CR><CMD>call setpos(".", p)<CR>', silent)
-        map('n', '<leader>es',  '<CMD>let p=getpos(".")<CR>vaE<CMD>EasyAlign *\\ <CR><CMD>call setpos(".", p)<CR>', silent)
-        map('n', '<leader>e--', '<CMD>let p=getpos(".")<CR>vaE<CMD>EasyAlign */ -- /<CR><CMD>call setpos(".", p)<CR>', silent)
-        map('v', '<leader>et',  '<CMD>EasyAlign * &<CR>', silent)
-        map('v', '<leader>es',  '<CMD>EasyAlign *\\ <CR>', silent)
-        map('v', '<leader>e--', '<CMD>EasyAlign */ -- /<CR>', silent)
+        map('n', '<leader>et',  '<CMD>let p=getpos(".")<CR>vie:EasyAlign * &<CR><CMD>call setpos(".", p)<CR>', silent)
+        map('n', '<leader>es',  '<CMD>let p=getpos(".")<CR>vaE:EasyAlign *\\ <CR><CMD>call setpos(".", p)<CR>', silent)
+        map('n', '<leader>e--', '<CMD>let p=getpos(".")<CR>vaE:EasyAlign */ -- /<CR><CMD>call setpos(".", p)<CR>', silent)
+        map('v', '<leader>et',  ':EasyAlign * &<CR>', silent)
+        map('v', '<leader>es',  ':EasyAlign *\\ <CR>', silent)
+        map('v', '<leader>e--', ':EasyAlign */ -- /<CR>', silent)
     end
 }
 
