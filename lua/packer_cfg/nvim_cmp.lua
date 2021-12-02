13ea511f6f7143bbdb9ed7fbfe274fbe2a225295
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-12-02 10:12:00 +0300

    minor

--- lua/packer_cfg/nvim_cmp.lua
+++ lua/packer_cfg/nvim_cmp.lua
@@ -24,17 +24,16 @@ return {
                 end,
             },
             mapping = {
-                --['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-                --['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-                --['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                -- ['<C-y>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                --['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-                --['<C-e>'] = cmp.mapping({
-                    --i = cmp.mapping.abort(),
-                    --c = cmp.mapping.close(),
-                --}),
-                --['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-y>'] = cmp.mapping.confirm({ select = false }),
+                ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+                ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+                -- ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+                ['<C-y>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+                -- ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
+                ['<C-e>'] = cmp.mapping({
+                    i = cmp.mapping.abort(),
+                    c = cmp.mapping.close(),
+                }),
+                -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
             },
             sources = cmp.config.sources({
                     -- { name = 'spell' },
