af629c4d3e74bbde3d6cfcc38405c2e04626ccdb
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-01-11 15:24:03 +0300

    minor

--- lua/packer_cfg/lualine.lua
+++ lua/packer_cfg/lualine.lua
@@ -1,9 +1,40 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  requires = 'kyazdani42/nvim-web-devicons',
-  config=function()
-      require 'lualine'.setup{
+    'nvim-lualine/lualine.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config=function()
 
-      }
-  end
+        local function keymap()
+            return vim.o.iminsert>0 and vim.o.keymap:sub(1, 2) or ''
+        end
+
+        local function paste()
+            return vim.o.paste and 'PASTE' or ''
+        end
+
+        require 'lualine'.setup{
+            sections = {
+                lualine_a = {
+                    'mode',
+                    keymap,
+                    {
+                        paste,
+                        separator = nil,
+                        color = { bg='red' }
+                    },
+                },
+                lualine_c = {
+                    {
+                        'filename',
+                        path = 1,
+                        -- shorting_target = 0
+                    }
+                },
+                lualine_x = {
+                    { 'encoding', cond = function() return vim.o.encoding~='utf-8' end },
+                    { 'fileformat', cond = function() return vim.o.fileformat~='unix' end },
+                    'filetype'
+                },
+            }
+        }
+    end
 }
