65954b536675d21b505a98b5bef333af4f2720a7
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-01-12 15:07:29 +0300

    minor

--- lua/packer_cfg/lualine.lua
+++ lua/packer_cfg/lualine.lua
@@ -2,13 +2,43 @@ return {
     'nvim-lualine/lualine.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
     config=function()
-
         local function keymap()
             return vim.o.iminsert>0 and vim.o.keymap:sub(1, 2) or ''
         end
 
         local function paste()
             return vim.o.paste and 'PASTE' or ''
+        end
+
+        local function vimtex()
+            local status={}
+
+            local vt_local = vim.b.vimtex_local
+            if vt_local then
+                table.insert(status, vt_local.active and 'l' or 'm')
+            end
+
+            local vimtex = vim.b.vimtex
+            if vimtex then
+                local viewer = vimtex.viewer
+                if viewer and viewer.xwin_id~=0 then
+                    table.insert(status, 'v')
+                end
+
+                local compiler = vimtex.compiler
+                if compiler and compiler.is_running~=vim.NIL then
+                    table.insert(status, compiler.continuous and '∞' or '1')
+                end
+            end
+
+            if #status==0 then
+                return ''
+            end
+
+            table.insert(status, 1, '{')
+            table.insert(status, '}')
+
+            return table.concat(status)
         end
 
         require 'lualine'.setup{
@@ -29,6 +59,7 @@ return {
                     }
                 },
                 lualine_x = {
+                    { vimtex, cond = function() return vim.o.filetype=='tex' end },
                     { 'encoding', cond = function() return vim.o.encoding~='utf-8' end },
                     { 'fileformat', cond = function() return vim.o.fileformat~='unix' end },
                     'filetype'
