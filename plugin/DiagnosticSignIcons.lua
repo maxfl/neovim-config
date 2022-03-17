82a693a57e17f27a6b8b8ff85eb48ab7f18cc90f
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-03-17 14:35:18 +0300

    minor

--- plugin/DiagnosticSignIcons.lua
+++ plugin/DiagnosticSignIcons.lua
@@ -1,4 +1,6 @@
-local signs = { Error = "", Warning = "", Hint = "", Info = "" }
+local signs = { Error = "", Warning = "", Hint = "", Info = "" }
+-- local signs = { Error = "", Warning = "", Hint = "💡", Info = "" }
+-- local signs = { Error = "", Warning = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
