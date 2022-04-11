c23cbf06f7cb25044c58bbdbefb4c19c60a8fbe0
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-04-11 10:12:54 +0300

    disable all (optionally)

--- lua/packer_cfg/packer.lua
+++ lua/packer_cfg/packer.lua
@@ -3,10 +3,53 @@ local packer=require 'packer'
 
 packer.init{
     opt_default=false,
+    max_jobs=8,
     git={
         clone_timeout=180
     }
 }
+
+function set_defaults(opts, extra)
+    if type(opts)~='table' then
+        return
+    end
+
+    local default_disable = false
+
+    if opts.disable==nil then
+        opts.disable = default_disable
+    end
+
+    if extra then
+        for k, v in pairs(extra) do
+            opts[k]=v
+        end
+    end
+end
+
+function useauto(arg, extra)
+    local use=packer.use
+    local tbl = nil
+    if type(arg)=='string' then
+        if arg:match '^packer_cfg%.' then
+            tbl = require(arg)
+        else
+            tbl = {arg}
+        end
+    else
+        tbl = arg
+    end
+
+    assert(type(tbl)=='table', 'expect table as configuration')
+
+    for i=1,#tbl do
+        set_defaults(tbl[i], extra)
+    end
+    set_defaults(tbl, extra)
+
+    use(tbl)
+end
+
 packer.startup(function()
     local use=packer.use
 -- Lua
@@ -15,248 +58,248 @@ packer.startup(function()
         use 'wbthomason/packer.nvim'
 
     -- Colocschemes
-        use (require 'packer_cfg.colorscheme')
+        useauto 'packer_cfg.colorscheme'
 
     -- Tree sitter
-        use (require 'packer_cfg.nvim_treesitter')
-        use {'romgrk/nvim-treesitter-context', requires='nvim-treesitter/nvim-treesitter'}
-        use (require 'packer_cfg.nvim_treesitter_textobjects')
-        -- use (require 'packer_cfg.nvim_treesitter_textsubjects')
-        use (require 'packer_cfg.nvim_treesitter_endwise')
+        useauto 'packer_cfg.nvim_treesitter'
+        useauto {'romgrk/nvim-treesitter-context', requires='nvim-treesitter/nvim-treesitter'}
+        useauto 'packer_cfg.nvim_treesitter_textobjects'
+        -- useauto 'packer_cfg.nvim_treesitter_textsubjects'
+        useauto 'packer_cfg.nvim_treesitter_endwise'
 
     -- LSP
-        use 'neovim/nvim-lspconfig'
-        use (require 'packer_cfg.nvim_lspinstall')
+        useauto 'neovim/nvim-lspconfig'
+        useauto 'packer_cfg.nvim_lspinstall'
 
-        --use {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}, config=function() require 'navigator'.setup() end}
-        use (require 'packer_cfg.codeactions')
-        use (require 'packer_cfg.symbols_outline')
-        use (require 'packer_cfg.diaglist')
-        use (require 'packer_cfg.trouble')
+        --useauto {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}, config=function() require 'navigator'.setup() end}
+        useauto 'packer_cfg.codeactions'
+        useauto 'packer_cfg.symbols_outline'
+        useauto 'packer_cfg.diaglist'
+        useauto 'packer_cfg.trouble'
 
     -- Libraries
-        use 'nvim-lua/plenary.nvim'
-        use {'ncm2/float-preview.nvim', config=function() vim.g['float_preview#docked']=1; vim.opt.completeopt:append 'preview' end}
+        useauto 'nvim-lua/plenary.nvim'
+        useauto {'ncm2/float-preview.nvim', config=function() vim.g['float_preview#docked']=1; vim.opt.completeopt:append 'preview' end}
 
     -- Editing
-        use (require 'packer_cfg.comment')
-        -- use (require 'packer_cfg.gomove')
+        useauto 'packer_cfg.comment'
+        -- useauto 'packer_cfg.gomove'
 
     -- Windows and floats, interface
-        use {'luukvbaal/stabilize.nvim', config = function() require("stabilize").setup() end, disable=true}
-        use (require 'packer_cfg.focus')
-        use (require 'packer_cfg.winshift')
-        use (require 'packer_cfg.nvim_tabline')
-        use (require 'packer_cfg.zen_mode')
+        useauto {'luukvbaal/stabilize.nvim', config = function() require("stabilize").setup() end, disable=true}
+        useauto 'packer_cfg.focus'
+        useauto 'packer_cfg.winshift'
+        useauto 'packer_cfg.nvim_tabline'
+        useauto 'packer_cfg.zen_mode'
 
-        use (require 'packer_cfg.bufdelete')
-        use (require 'packer_cfg.close_buffers')
+        useauto 'packer_cfg.bufdelete'
+        useauto 'packer_cfg.close_buffers'
 
-        use (require 'packer_cfg.indent_blankline')
+        useauto 'packer_cfg.indent_blankline'
 
-        -- use (require 'packer_cfg.FTerm')
-        use (require 'packer_cfg.toggleterm')
+        -- useauto 'packer_cfg.FTerm'
+        useauto 'packer_cfg.toggleterm'
 
     -- Menus
-        use (require 'packer_cfg.telescope')
-        -- use (require 'packer_cfg.telescope_rtp')
-        use (require 'packer_cfg.jabs')
+        useauto 'packer_cfg.telescope'
+        -- useauto 'packer_cfg.telescope_rtp'
+        useauto 'packer_cfg.jabs'
 
     -- Keys
-        use (require 'packer_cfg.whichkey')
+        useauto 'packer_cfg.whichkey'
 
     -- Motion
-        use (require 'packer_cfg.hop')
+        useauto 'packer_cfg.hop'
 
     -- Snippets
-        use (require 'packer_cfg.pears')
-        -- use (require 'packer_cfg.tabout')
+        useauto 'packer_cfg.pears'
+        -- useauto 'packer_cfg.tabout'
 
     -- Highlight
-        -- use 'yamatsum/nvim-cursorline'
-        use (require 'packer_cfg.cursorword')
-        -- use (require 'packer_cfg.beacon')
-        use (require 'packer_cfg.specs') -- hightlight cursor jump
-        use (require 'packer_cfg.shade')
-        -- use (require 'packer_cfg.illuminate') -- vim
+        -- useauto 'yamatsum/nvim-cursorline'
+        useauto 'packer_cfg.cursorword'
+        -- useauto 'packer_cfg.beacon'
+        useauto 'packer_cfg.specs' -- hightlight cursor jump
+        useauto 'packer_cfg.shade'
+        -- useauto 'packer_cfg.illuminate' -- vim
 
     -- Search and replace
-        use (require 'packer_cfg.nvim_spectre')
+        useauto 'packer_cfg.nvim_spectre'
 
     -- Clipboard
-        use 'machakann/vim-highlightedyank'
-        -- use (require 'packer_cfg.neoclip')
-        use (require 'packer_cfg.nvim_miniyank')
+        useauto 'machakann/vim-highlightedyank'
+        -- useauto 'packer_cfg.neoclip'
+        useauto 'packer_cfg.nvim_miniyank'
 
     -- VCS and Project
-        use {'lewis6991/gitsigns.nvim', config=function() require('gitsigns').setup() end, requires='nvim-lua/plenary.nvim'}
-        use (require 'packer_cfg.project')
+        useauto {'lewis6991/gitsigns.nvim', config=function() require('gitsigns').setup() end, requires='nvim-lua/plenary.nvim'}
+        useauto 'packer_cfg.project'
 
     -- Statusline
-        -- use (require 'packer_cfg.windline')
-        use (require 'packer_cfg.lualine')
+        -- useauto 'packer_cfg.windline'
+        useauto 'packer_cfg.lualine'
 
     -- Filetype
         -- lua
-        use {'rafcamlet/nvim-luapad', cmd = 'Luapad'}
-        -- use (require 'packer_cfg.lua_dev')
-        use (require 'packer_cfg.nlua')
-        use (require 'packer_cfg.glow')
+        useauto {'rafcamlet/nvim-luapad', cmd = 'Luapad'}
+        -- useauto 'packer_cfg.lua_dev'
+        useauto 'packer_cfg.nlua'
+        useauto 'packer_cfg.glow'
 
         -- terminal
-        use {'norcalli/nvim-terminal.lua', config=function() require 'terminal'.setup() end, ft='terminal'}
+        useauto {'norcalli/nvim-terminal.lua', config=function() require 'terminal'.setup() end, ft='terminal'}
 
         -- tex
-        -- use (require 'packer_cfg.nabla')
+        -- useauto 'packer_cfg.nabla'
 
     -- Files and folders
-        use {'jghauser/mkdir.nvim', config = function() require('mkdir') end}
-        use (require 'packer_cfg.nvim_tree')
-        use (require 'packer_cfg.firenvim')
+        useauto {'jghauser/mkdir.nvim', config = function() require('mkdir') end}
+        useauto 'packer_cfg.nvim_tree'
+        useauto 'packer_cfg.firenvim'
 
     -- Tools
-        use (require 'packer_cfg.neuron')
+        useauto 'packer_cfg.neuron'
 
     -- Completion
-        -- use (require 'packer_cfg.nvim_cmp')
-        use (require 'packer_cfg.coq_nvim')
+        -- useauto 'packer_cfg.nvim_cmp'
+        useauto ('packer_cfg.coq_nvim', {disable=false})
 
 -- Vimscript
     -- Libraries
-        use 'tpope/vim-repeat'
-        use 'inkarkat/vim-visualrepeat'
+        useauto 'tpope/vim-repeat'
+        useauto 'inkarkat/vim-visualrepeat'
 
     -- Indentation, encoding, settings
-        use {'thiagoalessio/rainbow_levels.vim', cmd = 'RainbowLevelsToggle'}
-        use {'kien/rainbow_parentheses.vim', cmd = 'RainbowParenthesesToggle'}
-        use {'ciaranm/detectindent', ft='python', config=function() vim.cmd[[au FileType cpp,python :DetectIndent]] end}
-        use (require 'packer_cfg.autofenc')
+        useauto {'thiagoalessio/rainbow_levels.vim', cmd = 'RainbowLevelsToggle'}
+        useauto {'kien/rainbow_parentheses.vim', cmd = 'RainbowParenthesesToggle'}
+        useauto {'ciaranm/detectindent', ft='python', config=function() vim.cmd[[au FileType cpp,python :DetectIndent]] end}
+        useauto 'packer_cfg.autofenc'
 
     -- Editing
-        use (require 'packer_cfg.vim_trailing_whitespace')
-        use (require 'packer_cfg.vim_visual_multi')
-        use 'kopischke/vim-fetch' -- jump to file:line:number
-        use (require 'packer_cfg.mundo')
-        use (require 'packer_cfg.textmanip')
+        useauto 'packer_cfg.vim_trailing_whitespace'
+        useauto 'packer_cfg.vim_visual_multi'
+        useauto 'kopischke/vim-fetch' -- jump to file:line:number
+        useauto 'packer_cfg.mundo'
+        useauto 'packer_cfg.textmanip'
 
     -- Characters and unicode
-        use 'chrisbra/unicode.vim'
-        use {'Konfekt/vim-unicode-homoglyphs', setup=function() vim.g.is_homoglyph_on=false end} -- search for hidden unicode characters
-        use (require 'packer_cfg.isotope')
+        useauto 'chrisbra/unicode.vim'
+        useauto {'Konfekt/vim-unicode-homoglyphs', setup=function() vim.g.is_homoglyph_on=false end} -- search for hidden unicode characters
+        useauto 'packer_cfg.isotope'
 
     -- Windows and buffers
-        use (require 'packer_cfg.choosewin')
-        use (require 'packer_cfg.winteract')
-        use (require 'packer_cfg.bufmru')
+        useauto 'packer_cfg.choosewin'
+        useauto 'packer_cfg.winteract'
+        useauto 'packer_cfg.bufmru'
 
     -- Motion
-        use {'inkarkat/vim-JumpToVerticalBlock', requires={{'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}}
-        use {'inkarkat/vim-JumpToVerticalOccurrence', requires={{'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}}
-        use {'inkarkat/vim-EnhancedJumps', config=require 'packer_cfg.EnhancedJumps', requires='inkarkat/vim-ingo-library'}
-        use 'tmhedberg/indent-motion'
-        -- use (require 'packer_cfg.easymotion')
-        use (require 'packer_cfg.cleverf')
-        use (require 'packer_cfg.camelcase')
+        useauto {'inkarkat/vim-JumpToVerticalBlock', requires={{'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}}
+        useauto {'inkarkat/vim-JumpToVerticalOccurrence', requires={{'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}}
+        useauto {'inkarkat/vim-EnhancedJumps', config=require 'packer_cfg.EnhancedJumps', requires='inkarkat/vim-ingo-library'}
+        useauto 'tmhedberg/indent-motion'
+        -- useauto 'packer_cfg.easymotion'
+        useauto 'packer_cfg.cleverf'
+        useauto 'packer_cfg.camelcase'
 
     -- Text objects
-        use (require 'packer_cfg.vim_textobj')
-        use (require 'packer_cfg.vim_cycle')
-        use (require 'packer_cfg.vim_sandwich')
-		use (require 'packer_cfg.matchup')
+        useauto 'packer_cfg.vim_textobj'
+        useauto 'packer_cfg.vim_cycle'
+        useauto 'packer_cfg.vim_sandwich'
+		useauto 'packer_cfg.matchup'
 
     -- Clipboard
-        use {'inkarkat/vim-UnconditionalPaste', requires={'inkarkat/vim-ingo-library', 'tpope/vim-repeat'} }
-        use {'inkarkat/vim-ReplaceWithRegister', requires={'tpope/vim-repeat', 'inkarkat/vim-visualrepeat'} }
-        use {'inkarkat/vim-RepeatableYank', requires={'inkarkat/vim-ingo-library', 'tpope/vim-repeat', 'inkarkat/vim-visualrepeat'} }
+        useauto {'inkarkat/vim-UnconditionalPaste', requires={'inkarkat/vim-ingo-library', 'tpope/vim-repeat'} }
+        useauto {'inkarkat/vim-ReplaceWithRegister', requires={'tpope/vim-repeat', 'inkarkat/vim-visualrepeat'} }
+        useauto {'inkarkat/vim-RepeatableYank', requires={'inkarkat/vim-ingo-library', 'tpope/vim-repeat', 'inkarkat/vim-visualrepeat'} }
 
     -- Menus
-        -- use (require 'packer_cfg.ctrlspace')
-        -- use (require 'packer_cfg.wilder') -- breaks highlighting
-        -- use (require 'packer_cfg.wheel')
+        -- useauto 'packer_cfg.ctrlspace'
+        -- useauto 'packer_cfg.wilder' -- breaks highlighting
+        -- useauto 'packer_cfg.wheel'
 
     -- Search and replace
-        use {'tpope/vim-abolish', cmd = 'S'} -- :%S////
-        use {'vim-scripts/ExplainPattern', cmd = 'ExplainPattern'}
-        use (require 'packer_cfg.quickhl')
-        use (require 'packer_cfg.searchhi')
-        use (require 'packer_cfg.vim_esearch') -- \ff
+        useauto {'tpope/vim-abolish', cmd = 'S'} -- :%S////
+        useauto {'vim-scripts/ExplainPattern', cmd = 'ExplainPattern'}
+        useauto 'packer_cfg.quickhl'
+        useauto 'packer_cfg.searchhi'
+        useauto 'packer_cfg.vim_esearch' -- \ff
 
     -- Diff tools
-        use (require 'packer_cfg.dirdiff')
-        use (require 'packer_cfg.linediff')
-        use {'rickhowe/diffchar.vim', cond='vim.o.diff'}
-        use {'rickhowe/spotdiff.vim', cond='vim.o.diff'} -- , cmd = {'Diffthis', 'Diffoff', 'Diffupdate'}}
-        -- use {'sindrets/diffview.nvim', cmd = 'Diffview*'}
-        use {'gangleri/vim-diffsaved', cmd = 'ToggleDiffSaved'}
-        use {'chrisbra/vim-diff-enhanced', cmd = 'EnhancedDiff'}
+        useauto 'packer_cfg.dirdiff'
+        useauto 'packer_cfg.linediff'
+        useauto {'rickhowe/diffchar.vim', cond='vim.o.diff'}
+        useauto {'rickhowe/spotdiff.vim', cond='vim.o.diff'} -- , cmd = {'Diffthis', 'Diffoff', 'Diffupdate'}}
+        -- useauto {'sindrets/diffview.nvim', cmd = 'Diffview*'}
+        useauto {'gangleri/vim-diffsaved', cmd = 'ToggleDiffSaved'}
+        useauto {'chrisbra/vim-diff-enhanced', cmd = 'EnhancedDiff'}
 
     -- Filetype
         -- tex
-        use {'KeitaNakamura/tex-conceal.vim', ft={'tex', 'plaintex'}}
-        use (require 'packer_cfg.vimtex')
+        useauto {'KeitaNakamura/tex-conceal.vim', ft={'tex', 'plaintex'}}
+        useauto 'packer_cfg.vimtex'
 
         -- julia
-        use 'JuliaEditorSupport/julia-vim'
-        use {'thinca/vim-prettyprint', cmd = {'PrettyPrint', 'PP'}}
+        useauto 'JuliaEditorSupport/julia-vim'
+        useauto {'thinca/vim-prettyprint', cmd = {'PrettyPrint', 'PP'}}
 
         -- others
-        use {'georgewitteman/vim-fish', ft='fish'}
-        use {'vim-scripts/ebnf.vim', ft='ebnf'} -- grammar
-        use {'plasticboy/vim-markdown', ft='markdown'}
-        use {'kalekundert/vim-coiled-snake', ft='python', setup=function() vim.g.coiled_snake_set_foldexpr=false end}
+        useauto {'georgewitteman/vim-fish', ft='fish'}
+        useauto {'vim-scripts/ebnf.vim', ft='ebnf'} -- grammar
+        useauto {'plasticboy/vim-markdown', ft='markdown'}
+        useauto {'kalekundert/vim-coiled-snake', ft='python', setup=function() vim.g.coiled_snake_set_foldexpr=false end}
 
     -- Spelling and grammar
-        use {'rhysd/vim-grammarous', cmd = {'GrammarousCheck', 'Grammarous*'}, config=function() vim.g['grammarous#languagetool_cmd']='languagetool' end}
+        useauto {'rhysd/vim-grammarous', cmd = {'GrammarousCheck', 'Grammarous*'}, config=function() vim.g['grammarous#languagetool_cmd']='languagetool' end}
 
     -- Terminal
-        use (require 'packer_cfg.neoterm')
+        useauto 'packer_cfg.neoterm'
 
     -- Snippets
-        use (require 'packer_cfg.ultisnips')
+        useauto 'packer_cfg.ultisnips'
 
     -- Files and folders
-        use (require 'packer_cfg.mirror')
-        use {'vim-scripts/Rename', cmd = 'Rename'}
-        use (require 'packer_cfg.sudoedit')
-        use 'ervandew/archive'
-        use (require 'packer_cfg.pushd')
+        useauto 'packer_cfg.mirror'
+        useauto {'vim-scripts/Rename', cmd = 'Rename'}
+        useauto 'packer_cfg.sudoedit'
+        useauto 'ervandew/archive'
+        useauto 'packer_cfg.pushd'
 
-        use {'https://gitlab.com/neonunux/vim-open-or-create-path-and-file.git', cmd = 'OpenOrCreateFile'}
+        useauto {'https://gitlab.com/neonunux/vim-open-or-create-path-and-file.git', cmd = 'OpenOrCreateFile'}
 
-        use (require 'packer_cfg.vim_fswitch')
-        use (require 'packer_cfg.rnvimr')
-        use (require 'packer_cfg.vim_external')
-        use (require 'packer_cfg.direnv')
+        useauto 'packer_cfg.vim_fswitch'
+        useauto 'packer_cfg.rnvimr'
+        useauto 'packer_cfg.vim_external'
+        useauto 'packer_cfg.direnv'
 
     -- VCS
-        use {'inkarkat/vim-ConflictDetection', requires='inkarkat/vim-ingo-library'}
-        use (require 'packer_cfg.conflictmotions')
-        use (require 'packer_cfg.fugitive')
-        -- use (require 'packer_cfg.vim_gista')
+        useauto {'inkarkat/vim-ConflictDetection', requires='inkarkat/vim-ingo-library'}
+        useauto 'packer_cfg.conflictmotions'
+        useauto 'packer_cfg.fugitive'
+        -- useauto 'packer_cfg.vim_gista'
 
     -- Tables and lists
-        use (require 'packer_cfg.bullets')
-        use (require 'packer_cfg.vim_table_mode')
-        use (require 'packer_cfg.vim_easy_align')
-        use (require 'packer_cfg.vim_transpose')
-        use {'inkarkat/vim-AdvancedSorters', cmd = 'SortVisible'}
-        use (require 'packer_cfg.vim_argwrap')
+        useauto 'packer_cfg.bullets'
+        useauto 'packer_cfg.vim_table_mode'
+        useauto 'packer_cfg.vim_easy_align'
+        useauto 'packer_cfg.vim_transpose'
+        useauto {'inkarkat/vim-AdvancedSorters', cmd = 'SortVisible'}
+        useauto 'packer_cfg.vim_argwrap'
 
     -- Documentation
-        use (require 'packer_cfg.zeavim')
-        use (require 'packer_cfg.devdocs')
+        useauto 'packer_cfg.zeavim'
+        useauto 'packer_cfg.devdocs'
 
     -- Local plugins
-        use {"~/.config/nvim/bundle_local/SwapText-1.02", keys={{'n', '<Leader>x'}, {'v', '<Leader>x'}}, requires={'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}
-        use {'~/.config/nvim/bundle_local/txt-vim-syntax-1.2', ft='txt'}
-        use {'~/.config/nvim/bundle_local/txtfmt_v3.1', ft='txt'}
+        useauto {"~/.config/nvim/bundle_local/SwapText-1.02", keys={{'n', '<Leader>x'}, {'v', '<Leader>x'}}, requires={'inkarkat/vim-CountJump', requires='inkarkat/vim-ingo-library'}}
+        useauto {'~/.config/nvim/bundle_local/txt-vim-syntax-1.2', ft='txt'}
+        useauto {'~/.config/nvim/bundle_local/txtfmt_v3.1', ft='txt'}
     end
 end)
 
 --[[
         -- Outdated
         -- Colorschemes
-        use {'mhartington/oceanic-next', config=function() vim.cmd"colorscheme OceanicNext" end}
+        useauto {'mhartington/oceanic-next', config=function() vim.cmd"colorscheme OceanicNext" end}
 
         -- Menus
         use('sudormrfbin/cheatsheet.nvim.git', { 'depends': 'telescope' })
