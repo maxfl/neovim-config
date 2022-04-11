fcadee36e7df7bfca132c4c4acaca9b2c74ad891
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-04-11 10:12:46 +0300

    coq_nvim configuration

--- lua/packer_cfg/coq_nvim.lua
+++ lua/packer_cfg/coq_nvim.lua
@@ -1,8 +1,29 @@
 return {
-    'ms-jpq/coq_nvim',
-    branch = 'coq',
-    requires = { {'ms-jpq/coq.artifacts', branch='artifacts'}, {'ms-jpq/coq.thirdparty', branch='3p'} },
-    setup = function()
-
-    end
+    {
+        'ms-jpq/coq_nvim',
+        branch = 'coq',
+        requires = { 'ms-jpq/coq.artifacts', 'ms-jpq/coq.thirdparty' },
+        config = function()
+            vim.g.coq_settings = {
+                auto_start = 'shut-up',
+                keymap = {
+                    manual_complete = '<c-x><c-a>'
+                }
+            }
+            require 'coq' ()
+        end
+    },
+    {
+        'ms-jpq/coq.thirdparty',
+        branch='3p',
+        config = function()
+            require 'coq_3p' {
+                { src = "nvimlua", short_name = "nLUA" },
+            }
+        end
+    },
+    {
+        'ms-jpq/coq.artifacts',
+        branch='artifacts'
+    }
 }
