cf9a185b658523882e7c351132f5cd4a52467efb
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-04-11 10:55:02 +0300

    coq: manual bindings

--- lua/packer_cfg/coq_nvim.lua
+++ lua/packer_cfg/coq_nvim.lua
@@ -6,11 +6,20 @@ return {
         config = function()
             vim.g.coq_settings = {
                 auto_start = 'shut-up',
-                keymap = {
-                    manual_complete = '<c-x><c-a>'
-                }
+                ['keymap.recommended'] = false,
+                ['keymap.pre_select']  = false,
+                ['keymap.manual_complete'] = '<c-x><c-a>',
             }
             require 'coq' ()
+
+            local map = vim.api.nvim_set_keymap
+            local opts = {silent=true, expr=true, noremap=true}
+            map('i', '<Esc>',   'pumvisible() ? "\\<C-e><Esc>" : "\\<Esc>"', opts)
+            map('i', '<C-c>',   'pumvisible() ? "\\<C-e><C-c>" : "\\<C-c>"', opts)
+            map('i', '<BS>',    'pumvisible() ? "\\<C-e><BS>"  : "\\<BS>"', opts)
+            -- map('i', '<M-CR>',  'pumvisible() ? (complete_info().selected == -1 ? "\\<C-e><CR>" : "\\<C-y>") : "\\<CR>"', opts)
+            map('i', '<Tab>',   'pumvisible() ? "\\<C-n>" : "\\<Tab>"', opts)
+            map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<BS>"', opts)
         end
     },
     {
