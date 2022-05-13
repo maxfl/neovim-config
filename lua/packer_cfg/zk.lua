de17d9a4cdf5108d93032b26d2ac47996c3630da
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-05-13 14:50:52 +0300

    zk

--- lua/packer_cfg/zk.lua
+++ lua/packer_cfg/zk.lua
@@ -1,5 +1,6 @@
 return {
     'mickael-menu/zk-nvim',
+    requires = 'folke/which-key.nvim',
     setup = function()
         vim.env['ZK_NOTEBOOK_DIR'] = '/home/gonchar/zettels'
     end,
@@ -7,5 +8,22 @@ return {
         require 'zk'.setup{
             picker = 'telescope'
         }
+
+        local status, whichkey=pcall(function() return require 'which-key' end)
+        if not status then return end
+
+        whichkey.register({
+            ['<leader>z'] = {
+                name = '+zettelkasten',
+                ['+'] = {'<CMD>ZkNew<CR>', 'new'},
+                l = {'<CMD>ZkLinks<CR>', 'links'},
+                n = {'<CMD>ZkNotes<CR>', 'notes'},
+                t = {'<CMD>ZkTags<CR>',  'tags'},
+            },
+            ['<leader>]'] = {
+                name = '+telescope',
+                z = {'<CMD>ZkLinks<CR>', 'zettelkasten'},
+            },
+        }, {silent=true})
     end
 }
