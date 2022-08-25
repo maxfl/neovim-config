84be1eadc1707e9c8af398143e5cfe5f3da4184c
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-08-25 16:25:33 +0300

    cfg: EnhancedJumps

--- lua/packer_cfg/EnhancedJumps.lua
+++ lua/packer_cfg/EnhancedJumps.lua
@@ -1,15 +1,20 @@
-return function()
-    vim.g.EnhancedJumps_CaptureJumpMessages=false
+return {
+    'inkarkat/vim-EnhancedJumps',
+    requires='inkarkat/vim-ingo-library',
+    disable = true,
+    config= function()
+        vim.g.EnhancedJumps_CaptureJumpMessages=false
 
-    local map, opts = vim.api.nvim_set_keymap, {}
-    -- map('n', '<Leader><C-O>', '<Plug>EnhancedJumpsOlder', opts)
-    -- map('n', '<Leader><C-I>', '<Plug>EnhancedJumpsNewer', opts)
-    map('n', '<C-O>',         '<Plug>EnhancedJumpsLocalOlder', opts)
-    map('n', '<C-I>',         '<Plug>EnhancedJumpsLocalNewer', opts)
+        local map, opts = vim.api.nvim_set_keymap, {}
+        map('n', '<Leader><C-O>', '<Plug>EnhancedJumpsOlder', opts)
+        map('n', '<Leader><C-I>', '<Plug>EnhancedJumpsNewer', opts)
+        map('n', '<C-O>',         '<Plug>EnhancedJumpsLocalOlder', opts)
+        map('n', '<C-I>',         '<Plug>EnhancedJumpsLocalNewer', opts)
 
-    map('n', '<M-n>',         '<Plug>EnhancedJumpsRemoteOlder', opts)
-    map('n', '<M-S-n>',       '<Plug>EnhancedJumpsRemoteNewer', opts)
+        map('n', '<M-n>',         '<Plug>EnhancedJumpsRemoteOlder', opts)
+        map('n', '<M-S-n>',       '<Plug>EnhancedJumpsRemoteNewer', opts)
 
-    -- map('n', '<M-n>', '<CMD>bnext<CR>', opts)
-    -- map('n', '<M-S-n>', '<CMD>bprev<CR>', opts)
-end
+        map('n', '<M-n>', '<CMD>bnext<CR>', opts)
+        map('n', '<M-S-n>', '<CMD>bprev<CR>', opts)
+    end
+}
