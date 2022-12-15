ad36ce588a06a4d4ce429710eb09a8b7bd20986b
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-12-15 17:37:18 +0300

    update

--- lua/packer_cfg/org/ctrlspace.lua
+++ lua/packer_cfg/org/ctrlspace.lua
@@ -1,6 +1,14 @@
 return {
     'vim-ctrlspace/vim-ctrlspace',
     setup = function()
-        vim.g.CtrlSpaceDefaultMappingKey = "<C-space> "
+        vim.g.CtrlSpaceSetDefaultMapping=true
+        vim.g.CtrlSpaceUseTabline=true
+        vim.g.CtrlSpaceDefaultMappingKey='<C-Space> '
+        vim.g.CtrlSpaceProjectRootMarkers = {".git", ".hg", ".svn", ".bzr", ".envrc", "_darcs", "CVS"}
+    end,
+    config = function()
+        local map = vim.api.nvim_set_keymap
+        map('n', '<M-u>',   '<CMD>CtrlSpaceGoUp<CR>', {})
+        map('n', '<M-S-u>', '<CMD>CtrlSpaceGoDown<CR>', {})
     end
 }
