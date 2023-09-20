e368f2c5c368fa36ce599ec059d628446ef9f017
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-09-20 10:41:47 +0300

    misc

--- lua/lazy_plugins/doc_zeavim.lua
+++ lua/lazy_plugins/doc_zeavim.lua
@@ -7,12 +7,10 @@ return {
     --     {'v', '<Leader>z'},
     --     -- {'n', '<Leader><Leader>z'}
     -- },
-    init = function()
+    config=function()
         vim.cmd[[
             au filetype python :Docset python,matplotlib,scipy,numpy,pandas
         ]]
-    end,
-    config=function()
         local map, opts=vim.api.nvim_set_keymap, {}
         map('n', '<Leader>z', '<Plug>Zeavim', opts)
         map('v', '<Leader>z', '<Plug>ZVVisSelection', opts)
