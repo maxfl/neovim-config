f5621969e25601695a8f02abec65f604a05a5014
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-09-26 17:40:03 +0300

    disable: wilder

--- lua/lazy_plugins/command-nvim-wilder.lua
+++ lua/lazy_plugins/command-nvim-wilder.lua
@@ -1,5 +1,7 @@
 return {
-	"gelguy/wilder.nvim",
+	-- "gelguy/wilder.nvim",
+    "KentoOgata/wilder.nvim",
+    enabled = false,
 	config = function()
 		vim.cmd([[
             call wilder#setup({'modes': [':']})
