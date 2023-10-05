967eb805146e20fe1ff480d611e2cad320f80c55
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-10-05 10:26:58 +0300

    update

--- lua/lazy_plugins/filetype-nvim-markdown-glow.lua
+++ lua/lazy_plugins/filetype-nvim-markdown-glow.lua
@@ -1,18 +1,16 @@
 return {
-    'ellisonleao/glow.nvim',
-    ft = 'markdown',
-    cmd = 'Glow',
-    init = function()
-        vim.cmd[[
+	"ellisonleao/glow.nvim",
+	init = function()
+		vim.cmd([[
             augroup UserMarkdown
                 au!
                 au BufEnter,BufNew *.md nmap <buffer> <Leader>og <CMD>Glow<CR>
                 au BufEnter,BufNew *.md nmap <buffer> <Leader>ov <CMD>!okular %&<CR>
             augroup END
-        ]]
-    end,
-    config = function()
-        vim.g.glow_binary_path = vim.env.HOME..'/.local/bin/'
-        vim.g.glow_border = 'rounded'
-    end
+        ]])
+	end,
+	opts = {
+		-- glow_path = vim.env.HOME .. "/.local/bin/",
+		border = "rounded",
+	},
 }
