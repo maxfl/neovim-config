b12f2a3c2b48bc3cf1433f6f8fc5271332bad29e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-10-26 11:09:24 +0300

    cfg: markdown * pair

--- lua/lazy_plugins/edit-nvim-pears.lua
+++ lua/lazy_plugins/edit-nvim-pears.lua
@@ -1,30 +1,35 @@
 return {
-    'steelsojka/pears.nvim',
-    priority = 57,
-    config=function()
-        local pears=require 'pears'
-        pears.setup(function(conf)
-            -- conf.pair('“', '”')
-            -- conf.pair('‘', '’')
-            -- conf.pair('„', '“')
-            -- conf.pair('«', '»')
+	"steelsojka/pears.nvim",
+	priority = 57,
+	config = function()
+		local pears = require("pears")
+		pears.setup(function(conf)
+			-- conf.pair('“', '”')
+			-- conf.pair('‘', '’')
+			-- conf.pair('„', '“')
+			-- conf.pair('«', '»')
 
-            --
-            -- Lua
-            --
-            conf.pair('[=[',  {close=']=]',  filetypes={'lua'}})
-            conf.pair('[==[', {close=']==]', filetypes={'lua'}})
-            -- conf.pair('function()', 'end')
+			--
+			-- Lua
+			--
+			conf.pair("[=[", { close = "]=]", filetypes = { "lua" } })
+			conf.pair("[==[", { close = "]==]", filetypes = { "lua" } })
+			-- conf.pair('function()', 'end')
 
-            --
-            -- Tex
-            --
-            conf.pair('$',  {close='$',  filetypes={'tex'}})
-            conf.pair(',,', {close='``', filetypes={'tex'}})
-            conf.pair('`',  {close="'",  filetypes={'tex'}})
-            conf.pair('``',  {close="''",  filetypes={'tex'}})
-            conf.pair('<<', {close='>>', filetypes={'tex'}})
-            conf.pair('\\(', {close='\\)', filetypes={'tex'}})
-        end)
-    end
+			--
+			-- Tex
+			--
+			conf.pair("$", { close = "$", filetypes = { "tex" } })
+			conf.pair(",,", { close = "``", filetypes = { "tex" } })
+			conf.pair("`", { close = "'", filetypes = { "tex" } })
+			conf.pair("``", { close = "''", filetypes = { "tex" } })
+			conf.pair("<<", { close = ">>", filetypes = { "tex" } })
+			conf.pair("\\(", { close = "\\)", filetypes = { "tex" } })
+
+			--
+			-- Markdown
+			--
+			conf.pair("\\*", { close = "\\*", filetypes = { "markdown" } })
+		end)
+	end,
 }
