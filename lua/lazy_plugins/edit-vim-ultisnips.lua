87227ec1be3e71cf9a3cfc2c67fcf215543d0a1e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-11-16 09:28:26 +0300

    chore: conditional ultisnips

--- lua/lazy_plugins/edit-vim-ultisnips.lua
+++ lua/lazy_plugins/edit-vim-ultisnips.lua
@@ -1,10 +1,13 @@
 return {
-    'SirVer/ultisnips',
-    dependencies = 'honza/vim-snippets',
-    init = function()
-        -- vim.g.UltiSnipsExpandTrigger='<Tab>'
-        vim.g.UltiSnipsExpandTrigger='<M-j>'
-        -- vim.g.UltiSnipsJumpForwardTrigger='<Tab>'
-        -- vim.g.UltiSnipsJumpBackwardTrigger='<S-Tab>'
-    end
+	"SirVer/ultisnips",
+	dependencies = "honza/vim-snippets",
+	cond = function()
+		return vim.fn.has("python3")
+	end,
+	init = function()
+		-- vim.g.UltiSnipsExpandTrigger='<Tab>'
+		vim.g.UltiSnipsExpandTrigger = "<M-j>"
+		-- vim.g.UltiSnipsJumpForwardTrigger='<Tab>'
+		-- vim.g.UltiSnipsJumpBackwardTrigger='<S-Tab>'
+	end,
 }
