b65ac299072eadae8ec372de9afdf9c7e77e424e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-11-16 12:21:17 +0300

    diffchar: set diffunit

--- lua/lazy_plugins/diff-vim-diffchar.lua
+++ lua/lazy_plugins/diff-vim-diffchar.lua
@@ -1,4 +1,9 @@
 return {
-    'rickhowe/diffchar.vim',
-    cond = function() return vim.o.diff end
+	"rickhowe/diffchar.vim",
+	cond = function()
+		return vim.o.diff
+	end,
+    init = function()
+        vim.g.DiffUnit = "Word"
+    end
 }
