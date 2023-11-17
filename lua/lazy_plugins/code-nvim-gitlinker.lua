0ed5196582718425e78b3d3b992d4776ddc7e107
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-11-17 14:20:51 +0300

    plug: gitlinker

---
+++ lua/lazy_plugins/code-nvim-gitlinker.lua
@@ -0,0 +1,52 @@
+return {
+	"linrongbin16/gitlinker.nvim",
+	config = function()
+		require("gitlinker").setup({
+			router = {
+				browse = {
+					["^git.jinr.ru"] = "https://git.jinr.ru/"
+						.. "{_A.USER}/"
+						.. "{_A.REPO}/blob/"
+						.. "{_A.REV}/"
+						.. "{_A.FILE}"
+						.. "#L{_A.LSTART}"
+						.. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+					["^code.ihep.ac.cn"] = "https://git.jinr.ru/"
+						.. "{_A.USER}/"
+						.. "{_A.REPO}/blob/"
+						.. "{_A.REV}/"
+						.. "{_A.FILE}"
+						.. "#L{_A.LSTART}"
+						.. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+				},
+				blame = {
+					["^git.jinr.ru"] = "https://git.jinr.ru/"
+						.. "{_A.USER}/"
+						.. "{_A.REPO}/blame/"
+						.. "{_A.REV}/"
+						.. "{_A.FILE}"
+						.. "#L{_A.LSTART}"
+						.. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+					["^code.ihep.ac.cn"] = "https://git.jinr.ru/"
+						.. "{_A.USER}/"
+						.. "{_A.REPO}/blame/"
+						.. "{_A.REV}/"
+						.. "{_A.FILE}"
+						.. "#L{_A.LSTART}"
+						.. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+				},
+			},
+		})
+		pcall(function()
+			require("which-key").register({
+				["<leader>g"] = {
+					name = "+git",
+					l = { "<CMD>GitLink<CR>", "copy link" },
+					L = { "<CMD>GitLink!<CR>", "open link" },
+				},
+			}, {
+				silent = true,
+			})
+		end)
+	end,
+}
