e371a08c29792ec106d20261ecbd6f92ccf63568
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-12-20 10:20:14 +0300

    enable hoverhints, disable mousemove

--- lua/lazy_plugins/lsp-nvim-hoverhints.lua
+++ lua/lazy_plugins/lsp-nvim-hoverhints.lua
@@ -1,5 +1,13 @@
 return {
-    'soulis-1256/hoverhints.nvim',
-    enabled = false,
-    config = true
+	"soulis-1256/hoverhints.nvim",
+	enabled = true,
+	config = function()
+		local hh = require("hoverhints")
+        hh.setup({})
+        local test = 1
+
+		vim.keymap.del("n", "<MouseMove>")
+		-- vim.keymap.set("n", "<MouseMove>", hh.show_diagnostics, { silent = true })
+        -- vim.api.nvim_del_augroup_by_name('ShowDiagnosticsOnModeChange')
+	end,
 }
