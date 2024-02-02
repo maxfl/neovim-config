c8c0e7e0fe02bc96de4c3c9718d7fb03cc1d2d4b
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-02-02 11:10:47 +0300

    update

--- lua/lazy_plugins/lsp-nvim-hoverhints.lua
+++ lua/lazy_plugins/lsp-nvim-hoverhints.lua
@@ -4,9 +4,8 @@ return {
 	config = function()
 		local hh = require("hoverhints")
         hh.setup({})
-        local test = 1
 
-		vim.keymap.del("n", "<MouseMove>")
+		-- vim.keymap.del("n", "<MouseMove>")
 		-- vim.keymap.set("n", "<MouseMove>", hh.show_diagnostics, { silent = true })
         -- vim.api.nvim_del_augroup_by_name('ShowDiagnosticsOnModeChange')
 	end,
