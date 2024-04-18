374b0d899e5e3c8c9c7a121b34cb69988655fb4e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-04-18 12:45:56 +0300

    update

--- lua/lazy_plugins/motion-vim-cleverf.lua
+++ lua/lazy_plugins/motion-vim-cleverf.lua
@@ -6,5 +6,20 @@ return {
         map('n', '<Leader>fr', '<Plug>(clever-f-reset)', noremap)
         map('n', ';', '<Plug>(clever-f-repeat-forward)', noremap)
         -- nnoremap , <Plug>(clever-f-repeat-back)\n
+
+        vim.cmd [[
+            nmap а <Plug>(clever-f-f)
+            xmap а <Plug>(clever-f-f)
+            omap а <Plug>(clever-f-f)
+            nmap А <Plug>(clever-f-F)
+            xmap А <Plug>(clever-f-F)
+            omap А <Plug>(clever-f-F)
+            nmap е <Plug>(clever-f-t)
+            xmap е <Plug>(clever-f-t)
+            omap е <Plug>(clever-f-t)
+            nmap Е <Plug>(clever-f-T)
+            xmap Е <Plug>(clever-f-T)
+            omap Е <Plug>(clever-f-T)
+        ]]
     end
 }
