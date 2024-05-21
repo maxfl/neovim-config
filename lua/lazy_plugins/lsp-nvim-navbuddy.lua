74f94ccc1e120951fce81915ab25824ab09a09ad
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-05-21 10:02:38 +0300

    update

--- lua/lazy_plugins/lsp-nvim-navbuddy.lua
+++ lua/lazy_plugins/lsp-nvim-navbuddy.lua
@@ -12,7 +12,7 @@ return {
 
 		navbuddy.setup({
 			lsp = {
-				auto_attach = true, -- If set to true, you don't need to manually use attach function
+				auto_attach = false, -- If set to true, you don't need to manually use attach function
 				preference = nil, -- list of lsp server names in order of preference
 			},
 		})
