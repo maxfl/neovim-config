dede7274009d42ff08be54b69913bbee9aa5d0d3
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-10-01 15:34:16 +0300

    chore: keys

--- lua/lazy_plugins/remote-vim-mirror.lua
+++ lua/lazy_plugins/remote-vim-mirror.lua
@@ -5,13 +5,13 @@ return {
 		pcall(function()
 			require("which-key").add({
 				{ "<leader>r", group = "mirror" },
-				{ "<Leader>t", "<CMD>MirrorPush<CR>", desc = "push (there)" },
-				{ "<Leader>c", "<CMD>MirrorPush<CR>", desc = "push" },
-				{ "<Leader>p", "<CMD>MirrorPull<CR>", desc = "pull" },
-				{ "<Leader>h", "<CMD>MirrorPull<CR>", desc = "pull (here)" },
-				{ "<Leader>s", "<CMD>MirrorSSH<CR>", desc = "ssh" },
-				{ "<Leader>C", "<CMD>MirrorConfig<CR>", desc = "config" },
-				{ "<Leader>r", "<CMD>MirrorConfigReload<CR>", desc = "reload config" },
+				{ "<Leader>rt", "<CMD>MirrorPush<CR>", desc = "push (there)" },
+				{ "<Leader>rc", "<CMD>MirrorPush<CR>", desc = "push" },
+				{ "<Leader>rp", "<CMD>MirrorPull<CR>", desc = "pull" },
+				{ "<Leader>rh", "<CMD>MirrorPull<CR>", desc = "pull (here)" },
+				{ "<Leader>rs", "<CMD>MirrorSSH<CR>", desc = "ssh" },
+				{ "<Leader>rC", "<CMD>MirrorConfig<CR>", desc = "config" },
+				{ "<Leader>rr", "<CMD>MirrorConfigReload<CR>", desc = "reload config" },
 			})
 		end)
 	end,
