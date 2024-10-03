98714cb173c0922c69a33fb1f929539a24f7a08d
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-10-03 09:38:30 +0300

    minor

--- lua/lazy_plugins/files-vim-open-or-create-path-and-file.lua
+++ lua/lazy_plugins/files-vim-open-or-create-path-and-file.lua
@@ -1,3 +1,6 @@
 return {
-    'https://gitlab.com/neonunux/vim-open-or-create-path-and-file.git'
+    'https://gitlab.com/neonunux/vim-open-or-create-path-and-file.git',
+    init = function()
+        vim.g.open_or_create_path_file_custom_keymap = false
+    end
 }
