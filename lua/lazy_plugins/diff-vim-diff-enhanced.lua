be84b3b6ac4ec34652fb736e8cdc609caf0b2b9a
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-10-14 16:34:37 +0300

    update

--- lua/lazy_plugins/diff-vim-diff-enhanced.lua
+++ lua/lazy_plugins/diff-vim-diff-enhanced.lua
@@ -1,3 +1,7 @@
 return {
-    'chrisbra/vim-diff-enhanced'
+    'chrisbra/vim-diff-enhanced',
+    config = function()
+        local map = vim.api.nvim_set_keymap
+        map('n', '<leader>gP', '<CMD>PatienceDiff<CR>', {silent=true})
+    end
 }
