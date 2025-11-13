3821753ad568b638ded5ff0b83dfdd119097881a
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-11-13 16:10:23 +0300

    update

---
+++ lua/lazy_plugins/util-nvim-atone.lua
@@ -0,0 +1,12 @@
+return {
+    "XXiaoA/atone.nvim",
+    cmd = "Atone",
+	keys = {
+		{
+			"<leader>uu",
+            "<CMD>Atone toggle<CR>",
+			desc = "undo history",
+		},
+	},
+    opts = {}, -- your configuration here
+}
